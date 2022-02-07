6e116ecb5ae67e1138401b3500f5159583228169
bilel00yacoubi <99219778+bilel00yacoubi@users.noreply.github.com>
2022-02-07 22:45:14 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+let myImage: Image = null
+music.playTone(262, music.beat(BeatFraction.Breve))
+basic.showString("Hello!")
+basic.clearScreen()
+basic.pause(50)
+basic.showIcon(IconNames.Heart)
+basic.clearScreen()
+myImage.showImage(0)
+basic.forever(function () {
+	
+})
